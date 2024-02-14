db3d16994bbf02378ebaa2f67aeeb3ba8a5d76cf
moskanoro <mostapha.boudad.02@gmail.com>
2024-02-15 01:34:04 +0800

    written by me

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the nunmber of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySql server.
+-- The database name will be passe as an argument of mysql command
+
+SELECT count(*) FROM first_table WHERE id=89;
